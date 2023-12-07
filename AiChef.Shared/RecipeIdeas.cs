4ccdf912ad4005f0d74bc6e2870ea6ce33ff023e
Leo Barnuevo <leocoding.net@gmail.com>
2023-12-07 07:10:57 -0600

    + Created MealIdeas component + Connected Generate button with MealIdeas component

---
+++ AiChef.Shared/RecipeIdeas.cs
@@ -0,0 +1,13 @@
+﻿namespace AiChef.Shared
+{
+    public class Idea
+    {
+
+        public int index { get; set; }
+
+        public string title { get; set; } = string.Empty;
+
+        public string description { get; set; } = string.Empty;
+
+    }
+}
