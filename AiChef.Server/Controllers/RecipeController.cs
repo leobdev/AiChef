2cbde97197ad090e9b17489d8b3bb2e17390da40
Leo Barnuevo <leocoding.net@gmail.com>
2023-12-08 07:02:00 -0600

    + Added RecipeState +Created state management for createmeal and mealideas + Added RecipeController + Added RecipeParms class to Shared

---
+++ AiChef.Server/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using AiChef.Server.Data;
+using AiChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas(RecipeParms recipeParms)
+        {
+            return  SampleData.RecipeIdeas;
+        }
+
+    }
+}
---
+++ AiChef.Shared/RecipeParms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiChef.Shared
+{
+    public class RecipeParms
+    {
+        public string MealTime { get; set; } = "Breakfast";
+        public List<Ingredient> Ingredients { get; set;} = new List<Ingredient>();
+        public string SelectedIdea { get; set; } = string.Empty;
+
+    }
+}
