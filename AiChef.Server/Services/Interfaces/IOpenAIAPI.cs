800d679a8a8e7f8d255f04406f44e61377f67e96
Leo Barnuevo <leocoding.net@gmail.com>
2023-12-09 06:40:59 -0600

    + Added IopenAIAPI & OpenAIService + Removed unnecessary class WeatherForecast &  WeatherForecastController + Created method to call endpoint in controller from MealIdeas component

---
+++ AiChef.Server/Services/Interfaces/IOpenAIAPI.cs
@@ -0,0 +1,9 @@
+﻿using AiChef.Shared;
+
+namespace AiChef.Server.Services.Interfaces
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+    }
+}
